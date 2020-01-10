7e55a21597bf040d4b145f93df60f62027defa3a
Andy Li <andy@onthewings.net>
2020-01-10 22:25:47 +0800

    more entities from 黃色文創圈 (WIP)

---
+++ src/withyoulike/entities/Eeedot.hx
@@ -0,0 +1,21 @@
+package withyoulike.entities;
+
+class Eeedot implements Entity {
+    public final name = [
+        en => "e eedot"
+    ];
+    public final webpages = [
+        {
+            url: "https://www.facebook.com/eeedot/"
+        }
+    ];
+    public final posts = [
+        {
+            url: "https://www.facebook.com/eeedot/photos/a.1634833706800704/2483821338568599/"
+        },
+        {
+            url: "https://www.facebook.com/eeedot/photos/a.1634833706800704/2401504173466983/"
+        }
+    ];
+}
+
