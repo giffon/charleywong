01e77d97bc991f313988d8da112ef7d2a53c9d9e
Andy Li <andy@onthewings.net>
2020-01-15 02:55:15 +0800

    Make entity indices lazy.

--- src/charleywong/EntityIndex.hx
+++ src/charleywong/EntityIndex.hx
@@ -4,12 +4,14 @@ using Lambda;
 using StringTools;
 
 class EntityIndex {
-    static public final entities:Map<String, Entity> = [
+    static public var entities(get, null):Map<String, Entity>;
+    static function get_entities() return entities != null ? entities : entities = [
         for (cls in CompileTime.getAllClasses("charleywong.entities", true, Entity))
         Type.getClassName(cls) => Type.createInstance(cls, [])
     ];
 
-    static public final entitiesOfUrl:Map<String, Entity> = [
+    static public var entitiesOfUrl(get, null):Map<String, Entity>;
+    static function get_entitiesOfUrl() return entitiesOfUrl != null ? entitiesOfUrl : entitiesOfUrl = [
         for (e in entities)
         for (page in e.webpages)
         page.url => e
@@ -17,7 +19,8 @@ class EntityIndex {
 
     static final fbPageRegexp = ~/^https:\/\/www\.facebook\.com\/(.+?)\/$/;
 
-    static public final entitiesOfFbPage:Map<String, Entity> = [
+    static public var entitiesOfFbPage(get, null):Map<String, Entity>;
+    static function get_entitiesOfFbPage() return entitiesOfFbPage != null ? entitiesOfFbPage : entitiesOfFbPage = [
         for (url => e in entitiesOfUrl)
         if (fbPageRegexp.match(url))
         fbPageRegexp.matched(1) => e
