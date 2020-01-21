c3425fe920b72e174c50bb031064f2d1b072c9f0
Andy Li <andy@onthewings.net>
2020-01-22 07:21:52 +0800

    opt for using http 1.0 when testing urls

--- test/TestAllEntities.hx
+++ test/TestAllEntities.hx
@@ -74,6 +74,7 @@ class TestAllEntities extends utest.Test {
             "-sSLf", url,
             "-o", "/dev/null",
             "--retry", "5",
+            "--http1.0",
             "-A", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
         ]);
         var code = p.exitCode();
