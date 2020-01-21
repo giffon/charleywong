d68a70f44e9819c2725ac0c79a921928d02442b7
Andy Li <andy@onthewings.net>
2020-01-21 21:24:23 +0800

    split NightOwl and Why50

--- src/charleywong/entities/NightOwlatWhy50.hx
+++ src/charleywong/entities/NightOwlatWhy50.hx
@@ -1,24 +1,11 @@
 package charleywong.entities;
 
 class NightOwlatWhy50 implements Entity {
-    public final id = "Why50hk";
+    public final id = "NightOwlatWhy50";
     public final name = [
         en => "NightOwl at Why50"
     ];
     public final webpages:Array<WebPage> = [
-        {
-            url: "https://www.facebook.com/Why50hk/",
-            meta: [
-                "about" => "Your Hip&Chill Coffee Neighbour",
-                "categories" => [
-                    "Coffee shop",
-                    "Restaurant"
-                ],
-                "addr" => "Shop E, G/F, Fu Fai Commercial Centre, 27 Hillier Street, Sheung Wan",
-                "area" => "Hong Kong",
-                "email" => "why50hk@gmail.com"
-            ]
-        },
         {
             url: "https://www.facebook.com/NightOwl-at-Why50-451834995354935/",
             meta: [
@@ -29,9 +16,6 @@ class NightOwlatWhy50 implements Entity {
                 ],
                 "email" => "night.owl.teamhk@gmail.com"
             ]
-        },
-        {
-            url: "https://www.instagram.com/why50hk/"
         }
     ];
     public final posts:Array<Post> = [
@@ -40,15 +24,6 @@ class NightOwlatWhy50 implements Entity {
         },
         {
             url: "https://www.facebook.com/permalink.php?story_fbid=641021229769643&id=451834995354935"
-        },
-        {
-            url: "https://www.facebook.com/Why50hk/photos/a.1499656653678701/2388323234812034/"
-        },
-        {
-            url: "https://www.facebook.com/Why50hk/posts/2347364062241285"
-        },
-        {
-            url: "https://www.facebook.com/Why50hk/photos/a.1499656653678701/2325969057714119/"
         }
     ];
 }
--- src/charleywong/entities/Why50Coffee.hx
+++ src/charleywong/entities/Why50Coffee.hx
@@ -5,12 +5,31 @@ class Why50Coffee implements Entity {
     public final name = [
         en => "Why 50 Coffee"
     ];
-    public final webpages = [
+    public final webpages:Array<WebPage> = [
         {
-            url: "https://www.facebook.com/Why50hk/"
+            url: "https://www.facebook.com/Why50hk/",
+            meta: [
+                "about" => "Your Hip&Chill Coffee Neighbour",
+                "categories" => [
+                    "Coffee shop",
+                    "Restaurant"
+                ],
+                "addr" => "Shop E, G/F, Fu Fai Commercial Centre, 27 Hillier Street, Sheung Wan",
+                "area" => "Hong Kong",
+                "email" => "why50hk@gmail.com"
+            ]
+        },
+        {
+            url: "https://www.instagram.com/why50hk/"
         }
     ];
-    public final posts = [
+    public final posts:Array<Post> = [
+        {
+            url: "https://www.facebook.com/Why50hk/photos/a.1499656653678701/2388323234812034/"
+        },
+        {
+            url: "https://www.facebook.com/Why50hk/posts/2347364062241285"
+        },
         {
             url: "https://www.facebook.com/Why50hk/photos/a.1499656653678701/2325969057714119/"
         }
