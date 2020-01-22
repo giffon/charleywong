6b016ede28d7827fd39ffc0bd423a0751505fb2f
Andy Li <andy@onthewings.net>
2020-01-22 16:57:48 +0800

    add Top Blade Steak Lab

---
+++ src/charleywong/entities/TopBladeSteakLab.hx
@@ -0,0 +1,34 @@
+package charleywong.entities;
+
+class TopBladeSteakLab implements Entity {
+    public final id = "topbladesteaklab";
+    public final name = [
+        en => "Top Blade Steak Lab"
+    ];
+    public final webpages:Array<WebPage> = [
+        {
+            url: "http://www.topbladesteak.com"
+        },
+        {
+            url: "https://www.facebook.com/topbladesteaklab/",
+            meta: [
+                "about" => "HMT:\n4A Soares Ave, Ho Man Tin / Mong Kok East\n+852 3956 2011...\n\n\n\n\n\nSee more",
+                "categories" => [
+                    "Steakhouse"
+                ]
+            ]
+        },
+        {
+            url: "https://www.instagram.com/topbladesteaklab/"
+        }
+    ];
+    public final posts:Array<Post> = [
+        {
+            url: "https://www.facebook.com/topbladesteaklab/posts/773033616540147"
+        },
+        {
+            url: "https://www.facebook.com/topbladesteaklab/posts/761532861023556"
+        }
+    ];
+}
+
