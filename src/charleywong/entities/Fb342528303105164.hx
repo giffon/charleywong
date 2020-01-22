1519a40b35df1fc76acfd0be0b89d412f213cb3a
Andy Li <andy@onthewings.net>
2020-01-23 03:20:22 +0800

    add 2 entities

---
+++ src/charleywong/entities/Fb342528303105164.hx
@@ -0,0 +1,33 @@
+package charleywong.entities;
+
+class Fb342528303105164 implements Entity {
+    public final id = "342528303105164";
+    public final name = [
+        zh => "蔥仔越南包"
+    ];
+    public final webpages:Array<WebPage> = [
+        {
+            url: "https://www.facebook.com/342528303105164/",
+            meta: [
+                "categories" => [
+                    "Vietnamese restaurant"
+                ],
+                "addr" => "櫸樹街52號",
+                "area" => "Hong Kong",
+                "email" => "banhmichung@gmail.com",
+                "tel" => "35905561"
+            ]
+        }
+    ];
+    public final posts:Array<Post> = [
+        {
+            url: "https://www.facebook.com/342528303105164/photos/a.349377322420262/459486738075986/"
+        },
+        {
+            url: "https://www.facebook.com/342528303105164/photos/a.349377322420262/449969379027722/"
+        },
+        {
+            url: "https://www.facebook.com/342528303105164/photos/a.349377322420262/423373838353943/"
+        }
+    ];
+}
---
+++ src/charleywong/entities/KKBOX.hx
@@ -0,0 +1,31 @@
+package charleywong.entities;
+
+class KKBOX implements Entity {
+    public final id = "KKBOXHK";
+    public final name = [
+        en => "KKBOX"
+    ];
+    public final webpages:Array<WebPage> = [
+        {
+            url: "http://www.kkbox.com"
+        },
+        {
+            url: "https://www.facebook.com/KKBOXHK/",
+            meta: [
+                "about" => "★KKBOX 一起聽音樂吧！ www.kkbox.com★",
+                "categories" => [
+                    "Product/service"
+                ]
+            ]
+        }
+    ];
+    public final posts:Array<Post> = [
+        {
+            url: "https://www.facebook.com/KKBOXHK/posts/10157139860099870"
+        },
+        {
+            url: "https://www.facebook.com/KKBOXHK/posts/10157367957979870"
+        }
+    ];
+}
+
