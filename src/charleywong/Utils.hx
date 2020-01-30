5d91e82e5a108002f0e0687b21eb73588632af61
Andy Li <andy@onthewings.net>
2020-01-31 05:49:46 +0800

    use higher connect time-out

--- src/charleywong/Utils.hx
+++ src/charleywong/Utils.hx
@@ -12,7 +12,7 @@ class Utils {
                 "-sSLkf", url,
                 "-o", "/dev/null",
                 "--retry", "3",
-                "--connect-timeout", "10",
+                "--connect-timeout", "20",
                 "--http1.0",
                 "-A", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
             ]);
