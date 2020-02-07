9faadd2a1a27aa836029da8602e8f6ce246189dc
stella <stella_paradise@yahoo.com.hk>
2020-02-08 02:48:20 +0800

    Add favicons

--- src/charleywong/views/View.hx
+++ src/charleywong/views/View.hx
@@ -63,6 +63,28 @@ class View extends ReactComponent {
         </Fragment>
     ');
 
+    function favicon() return jsx('
+        <Fragment>
+            <link rel="apple-touch-icon" sizes="57x57" href="/images/fav/apple-icon-57x57.png"/>
+            <link rel="apple-touch-icon" sizes="60x60" href="/images/fav/apple-icon-60x60.png"/>
+            <link rel="apple-touch-icon" sizes="72x72" href="/images/fav/apple-icon-72x72.png"/>
+            <link rel="apple-touch-icon" sizes="76x76" href="/images/fav/apple-icon-76x76.png"/>
+            <link rel="apple-touch-icon" sizes="114x114" href="/images/fav/apple-icon-114x114.png"/>
+            <link rel="apple-touch-icon" sizes="120x120" href="/images/fav/apple-icon-120x120.png"/>
+            <link rel="apple-touch-icon" sizes="144x144" href="/images/fav/apple-icon-144x144.png"/>
+            <link rel="apple-touch-icon" sizes="152x152" href="/images/fav/apple-icon-152x152.png"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/images/fav/apple-icon-180x180.png"/>
+            <link rel="icon" type="image/png" sizes="192x192"  href="/images/fav/android-icon-192x192.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/images/fav/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="96x96" href="/images/fav/favicon-96x96.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/images/fav/favicon-16x16.png"/>
+            <link rel="manifest" href="/images/fav/manifest.json"/>
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="msapplication-TileImage" content="/images/fav/ms-icon-144x144.png" />
+            <meta name="theme-color" content="#ffffff" />
+        </Fragment>
+    ');
+
     function head() return jsx('
         <head>
             ${gtag()}
@@ -70,6 +92,7 @@ class View extends ReactComponent {
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <title>${title()}</title>
+            ${favicon()}
             ${descriptionTag()}
             ${depCss()}
             ${depJs()}
