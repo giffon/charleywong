5129fb18defd2ff33a3fa4d249f10f7a08ec4210
Andy Li <andy@onthewings.net>
2020-02-10 17:08:58 +0800

    create InstagramImporter

--- src/charleywong/importer/Importer.hx
+++ src/charleywong/importer/Importer.hx
@@ -5,6 +5,7 @@ import haxe.io.*;
 import sys.io.File;
 import charleywong.*;
 import charleywong.importer.FacebookImporter;
+import charleywong.importer.InstagramImporter;
 using StringTools;
 using Lambda;
 
@@ -20,16 +21,7 @@ class Importer {
         importFbPage(fbPage, url);
     }
 
-    static function importFbPage(fbPage:String, postUrl:Null<String>) {
-        var entity = switch (entityIndex.entitiesOfFbPage[fbPage]) {
-            case null:
-                var importer = new FacebookImporter();
-                var info = importer.fbPageInfo(fbPage);
-                importer.destroy();
-                createEntity(info.name, info, postUrl);
-            case entity:
-                updateEntity(entity, postUrl);
-        }
+    static function saveEntity(entity:Entity) {
         var fileContent = haxe.Json.stringify(entity.toJson(), null, "  ");
         if (Sys.getEnv("CI") != null || Sys.getEnv("GITHUB_ACTIONS") != null) {
             Sys.println("In CI, skip writing file.");
@@ -42,34 +34,65 @@ class Importer {
         }
     }
 
+    static function importFbPage(fbPage:String, postUrl:Null<String>) {
+        var entity = switch (entityIndex.entitiesOfFbPage[fbPage]) {
+            case null:
+                var importer = new FacebookImporter();
+                var info = importer.fbPageInfo(fbPage);
+                importer.destroy();
+                createEntityFromFb(info, postUrl);
+            case entity:
+                updateEntity(entity, postUrl);
+        }
+        saveEntity(entity);
+    }
+
+    static function importIg(postUrl:Null<String>) {
+        var importer = new InstagramImporter();
+        var handle = importer.igHandle(postUrl);
+        var info = importer.igInfo(handle);
+        importer.destroy();
+
+        var entity = switch (entityIndex.entitiesOfUrl['https://www.instagram.com/${info.handle}/']) {
+            case null:
+                createEntityFromIg(info, postUrl);
+            case entity:
+                updateEntity(entity, postUrl);
+        }
+        saveEntity(entity);
+    }
+
     static function importUrl(url:String):Void {
-        if (!url.startsWith("https://www.facebook.com/")) {
-            throw '$url doesn\'t look like a Facebook URL.';
+        if (url.startsWith("https://www.facebook.com/")) {
+            // https://www.facebook.com/permalink.php?story_fbid=1384711015018289&id=897763637046365&__xts__%5B0%5D=68.ARDoat1gIoDWNhWOYOl4y2ph9GCqrYoWLE4bfOiLDxX9nOIYWW4IpujBgs52jBUlU_uzyAXHdsYldWW0tJvlRn-2_LXjLeAhbBIPxQLD1OXEoFFu1cmGbGm2XcVKvSI5yxeyLT-36li2qYQEPITHxhEMvwQAXYtySy_ErDCAPRi32K6H8iRp7pfc_pxc2S9AOb9IhlWwizb3o7_IunO6z-q22DorIPRXBBt2-R3Cpg4cz_yfM3-4uaGu2lPozkQXzcEwkCr84sUkl8gEVFaOYS8P51Xqo0jsH4gOHxwJP5c7hkyqxNvUmQhCjzFoticWkZ_p5laKOD5p0kh1bH3JgyMS7A&__tn__=-R
+            var permalinkRegexp = ~/^https:\/\/www\.facebook\.com\/permalink\.php.+$/;
+            if (permalinkRegexp.match(url)) {
+                var url = switch (url.indexOf("&__xts__")) {
+                    case -1:
+                        url;
+                    case qIndex:
+                        url.substring(0, qIndex);
+                };
+                importFbPermalink(url);
+                return;
+            }
+
+            var postRegexp = ~/^https:\/\/www\.facebook\.com\/(.+?)\/(?:posts|photos|videos)\/.+$/;
+            if (postRegexp.match(url)) {
+                var fbPage = postRegexp.matched(1);
+                var url = switch (url.indexOf("?")) {
+                    case -1:
+                        url;
+                    case qIndex:
+                        url.substring(0, qIndex);
+                };
+                importFbPage(fbPage, url);
+                return;
+            }
         }
 
-        // https://www.facebook.com/permalink.php?story_fbid=1384711015018289&id=897763637046365&__xts__%5B0%5D=68.ARDoat1gIoDWNhWOYOl4y2ph9GCqrYoWLE4bfOiLDxX9nOIYWW4IpujBgs52jBUlU_uzyAXHdsYldWW0tJvlRn-2_LXjLeAhbBIPxQLD1OXEoFFu1cmGbGm2XcVKvSI5yxeyLT-36li2qYQEPITHxhEMvwQAXYtySy_ErDCAPRi32K6H8iRp7pfc_pxc2S9AOb9IhlWwizb3o7_IunO6z-q22DorIPRXBBt2-R3Cpg4cz_yfM3-4uaGu2lPozkQXzcEwkCr84sUkl8gEVFaOYS8P51Xqo0jsH4gOHxwJP5c7hkyqxNvUmQhCjzFoticWkZ_p5laKOD5p0kh1bH3JgyMS7A&__tn__=-R
-        var permalinkRegexp = ~/^https:\/\/www\.facebook\.com\/permalink\.php.+$/;
-        if (permalinkRegexp.match(url)) {
-            var url = switch (url.indexOf("&__xts__")) {
-                case -1:
-                    url;
-                case qIndex:
-                    url.substring(0, qIndex);
-            };
-            importFbPermalink(url);
-            return;
-        }
-
-        var postRegexp = ~/^https:\/\/www\.facebook\.com\/(.+?)\/(?:posts|photos|videos)\/.+$/;
-        if (postRegexp.match(url)) {
-            var fbPage = postRegexp.matched(1);
-            var url = switch (url.indexOf("?")) {
-                case -1:
-                    url;
-                case qIndex:
-                    url.substring(0, qIndex);
-            };
-            importFbPage(fbPage, url);
+        if (url.startsWith("https://www.instagram.com/p/")) {
+            importIg(url);
             return;
         }
 
@@ -120,24 +143,6 @@ class Importer {
         return uppercaseFirstChar(removeNonEngChar(str));
     }
 
-    static function getClassName(name:String, fbPage:String):String {
-        if (~/^[^\u4e00-\u9fff]+$/.match(name)) {
-            return toTitleCase(name);
-        }
-
-        if (~/^[0-9]+$/.match(fbPage)) {
-            return 'Fb${fbPage}';
-        }
-
-        if (~/^[^\u4e00-\u9fff]+$/.match(fbPage)) {
-            return toTitleCase(fbPage);
-        }
-
-        return toTitleCase(name);
-
-        throw 'Cannot get a class name from $name ($fbPage).';
-    }
-
     static function updateEntity(entity:Entity, post:Null<String>):Entity {
         if (post == null || entity.posts.exists(p -> p.url == post)) {
             return entity;
@@ -150,34 +155,61 @@ class Importer {
     static final noChi = ~/^[^\u4e00-\u9fff]+$/; // no chinese characters
     static final allChi = ~/^[\u4e00-\u9fff \-_\.·]+$/; // all chinese characters
 
-    static function createEntity(name:String, fbPage:FacebookInfo, post:Null<String>):Entity {
-        var className = getClassName(name, fbPage.page);
-        var name:MultiLangString = {
-            var chi_en = ~/^([\u4e00-\u9fff ]*[\u4e00-\u9fff])[^A-Za-z0-9\u4e00-\u9fff]*([^\u4e00-\u9fff]+)$/; // chinese then eng
-            var en_chi = ~/^([^\u4e00-\u9fff]+?)[ \-]*([\u4e00-\u9fff]+)$/; // chinese then eng
-            if (noChi.match(name))
-                {
-                    en: name
-                }
-            else if (allChi.match(name))
-                {
-                    zh: name
-                }
-            else if (chi_en.match(name))
-                {
-                    zh: chi_en.matched(1),
-                    en: chi_en.matched(2)
-                }
-            else if (en_chi.match(name))
-                {
-                    en: en_chi.matched(1),
-                    zh: en_chi.matched(2)
-                }
-            else
-                {
-                    zh: name
-                }
+    static function parseName(name:String):MultiLangString {
+        var chi_en = ~/^([\u4e00-\u9fff ]*[\u4e00-\u9fff])[^A-Za-z0-9\u4e00-\u9fff]*([^\u4e00-\u9fff]+)$/; // chinese then eng
+        var en_chi = ~/^([^\u4e00-\u9fff]+?)[ \-]*([\u4e00-\u9fff]+)$/; // chinese then eng
+        return if (noChi.match(name))
+            {
+                en: name
+            }
+        else if (allChi.match(name))
+            {
+                zh: name
+            }
+        else if (chi_en.match(name))
+            {
+                zh: chi_en.matched(1),
+                en: chi_en.matched(2)
+            }
+        else if (en_chi.match(name))
+            {
+                en: en_chi.matched(1),
+                zh: en_chi.matched(2)
+            }
+        else
+            {
+                zh: name
+            }
+    }
+
+    static function createEntityFromIg(igInfo:InstagramInfo, post:Null<String>):Entity {
+        var webpages:Array<WebPage> = [];
+        if (igInfo.website != null) {
+            webpages.push({
+                url: igInfo.website,
+            });
+        }
+        var meta:DynamicAccess<Dynamic> = {};
+        if (igInfo.about != null) {
+            meta["about"] = igInfo.about;
+        }
+        webpages.push({
+            url: 'https://www.instagram.com/${igInfo.handle}/',
+            meta: meta,
+        });
+        return {
+            id: igInfo.handle,
+            name: parseName(igInfo.name),
+            webpages: webpages,
+            posts: [{
+                url: post
+            }],
+            tags: [],
         };
+    }
+
+    static function createEntityFromFb(fbPage:FacebookInfo, post:Null<String>):Entity {
+        var name = parseName(fbPage.name);
         var id = fbPage.page;
         var posts:Array<Post> = post == null ? [] : [{
             url: post,
---
+++ src/charleywong/importer/InstagramImporter.hx
@@ -0,0 +1,105 @@
+package charleywong.importer;
+
+import selenium.webdriver.*;
+import selenium.webdriver.common.alert.Alert;
+import selenium.webdriver.common.action_chains.ActionChains;
+import selenium.webdriver.support.ui.Select;
+import selenium.webdriver.support.ui.WebDriverWait;
+import selenium.webdriver.support.expected_conditions.*;
+import selenium.webdriver.remote.switch_to.SwitchTo;
+import selenium.webdriver.remote.webelement.*;
+using StringTools;
+using Lambda;
+
+typedef InstagramInfo = {
+    handle:String,
+    name:String,
+    about:String,
+    website:String,
+};
+
+class InstagramImporter {
+    final driver:Remote;
+    final hubHost:String;
+    final hubUrl:String;
+
+    public function new():Void {
+        hubHost = arg("HUB_HOST", "localhost");
+        hubUrl = 'http://${hubHost}:4444/wd/hub';
+
+
+        var opts = new ChromeOptions();
+        opts.add_argument("--disable-dev-shm-usage");
+        opts.set_capability("acceptSslCerts", true);
+        driver = new Remote(hubUrl, opts.to_capabilities());
+    }
+
+    public function destroy():Void {
+        driver.quit();
+    }
+
+    function arg(name:String, defaultValue:String) {
+        return switch (Sys.getEnv(name)) {
+            case null: defaultValue;
+            case v: v;
+        }
+    }
+
+    // Get Instagram handle from an Instagram post link.
+    public function igHandle(url:String):String {
+        if (!url.startsWith("https://www.instagram.com/p/")) {
+            throw 'Not an Instagram post? $url';
+        }
+
+        driver.get(url);
+
+        var h2:WebElement = driver.find_element_by_xpath("//article//header//h2//a");
+        var href:String = h2.get_attribute("href");
+        var regexp = ~/^https:\/\/www\.instagram\.com\/([^\/]+)\/$/;
+        if (regexp.match(href)) {
+            return regexp.matched(1);
+        } else {
+            throw '$href is not an Instagram profile?';
+        }
+    }
+
+    function igName() {
+        var h1s:Array<WebElement> = driver.find_elements_by_xpath("//main//header//h1");
+        var nameElement = h1s[1];
+        return nameElement.text;
+    }
+
+    function igWebsite() {
+        var as:Array<WebElement> = driver.find_elements_by_xpath("//main//header//a[contains(@rel,'me')]");
+        switch (as) {
+            case []:
+                return null;
+            case [a]:
+                var href:String = a.get_attribute("href");
+                var regexp = ~/^https:\/\/l\.instagram\.com\/\?u=([^&]+).*$/;
+                if (regexp.match(href)) {
+                    return regexp.matched(1).urlDecode();
+                } else {
+                    throw 'Cannot parse $href';
+                }
+            case _:
+                throw 'There are more then 1 matched links.';
+        }
+    }
+
+    function igAbout() {
+        var h1s:Array<WebElement> = driver.find_elements_by_xpath("//main//header//h1");
+        var aboutElement:WebElement = h1s[1].find_element_by_xpath("../span");
+        return aboutElement.text;
+    }
+
+    public function igInfo(igHandle:String):InstagramInfo {
+        driver.get('https://www.instagram.com/${igHandle}/');
+        return {
+            handle: igHandle,
+            name: igName(),
+            website: igWebsite(),
+            about: igAbout(),
+        };
+    }
+}
