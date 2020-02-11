0ab7f6ff88431b40ecc2052489390ed34752e78f
stella <stella_paradise@yahoo.com.hk>
2020-02-11 23:04:13 +0800

    Add intro to index

--- src/charleywong/views/Index.hx
+++ src/charleywong/views/Index.hx
@@ -10,7 +10,7 @@ class Index extends View {
 
     static public function searchForm(query:String) return jsx('
         <form className="d-flex" action="/">
-            <input className="w-100 p-3 search-input" type="text" name="search" placeholder="商業/品牌名稱" defaultValue=${query} autoFocus=${true} />
+            <input className="w-100 p-3 search-input" type="text" name="search" placeholder="商業/專頁名稱" defaultValue=${query} autoFocus=${true} />
             <input className="btn m-0 p-3 search-btn" type="submit" value="🔎" />
         </form>
     ');
@@ -20,13 +20,34 @@ class Index extends View {
             <Fragment>
                 <div className="mt-5 container">
                     <div className="row justify-content-center">
-                        <div className="col-12 col-md-6 col-lg-4 text-center">
+                        <div className="col-12 col-md-10 col-lg-4 text-center">
                             <img className="w-100" src="/images/logo4.png" alt="Charley Wong"/>
                             ${searchForm("")}
                             <img className="w-100" src="/images/logo-b-gif.gif" alt="和你查"/>
                             
                             <div className="mt-3">
                                 <a href="list/all">查看完整名單</a>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-md-6 col-lg-4 order-first-lg">
+                            <div>
+                                <img className="w-100 p-3" src="/images/circle.png"/>
+                            </div>
+                            <div className="text-center px-5 pb-5 py-md-0 color_black_o60">
+                                <div className="container-yc"><span className="yc">黃</span><span className="yc">色</span><span className="yc">經</span><span className="yc">濟</span><span className="yc">圈</span></div>
+                                <p className="p-3">Charley Wong 和你查 搜集多個商店、創作人、專頁，以第一身表態支持 #香港人 #反送中，爭取 #自由 #民主 的良心發言。當中有高調行動，有低調暗示，我地不作篩選，各位自行以個人喜好辨別。</p>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-md-6 col-lg-4">
+                            <div>
+                                <img className="w-100 p-3" src="/images/open.png"/>
+                            </div>
+                            <div className="text-center px-5 color_black_o60">
+                                <div><span className="yc">開</span><span className="yc">放</span><span className="yc">數</span><span className="yc">據</span></div>
+                                <div><span className="yc">開</span><span className="yc">放</span><span className="yc">原</span><span className="yc">碼</span></div>
+                                <p className="p-3">Charley Wong 和你查 以高度透明同公開嘅方式運作，而且鼓勵同路人使用我地嘅成果：我地以 open license 發佈所有數據同原碼。任何人只要註明出處，都可以隨意使用。</p>
                             </div>
                         </div>
                     </div>
