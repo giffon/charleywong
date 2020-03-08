4b9ac6f97733863ad9192a2290dfc417df8a1862
Andy Li <andy@onthewings.net>
2020-03-08 18:11:31 +0800

    there is fb.com as well

--- src/charleywong/UrlExtractors.hx
+++ src/charleywong/UrlExtractors.hx
@@ -142,7 +142,7 @@ class UrlExtractors {
     }
 
     static public function extractFbHomePage(url:ParsedUrl) {
-        var regex = ~/^https?:\/\/(?:www\.|m\.)?(?:facebook\.com|fb\.me)$/i;
+        var regex = ~/^https?:\/\/(?:www\.|m\.)?(?:facebook\.com|fb\.me|fb\.com)$/i;
         if (regex.match(url.origin)) {
             var handle = switch(url.pathname.split("/")) {
                 case ["", handle]: handle;
