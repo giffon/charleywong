bac43677873639163b2550da5435e82d47ca6bc3
Andy Li <andy@onthewings.net>
2020-08-05 19:53:29 +0800

    put devcontainer postCreateCommand in its own file, use a volume for node_modules

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+python3 -m virtualenv -p /usr/bin/python3.7 ~/venv
+
+echo "source ~/venv/bin/activate" >> ~/.bashrc
+bash -ic "pip3 install -r requirements.txt"
+
+sudo chmod a+rwx node_modules
+yarn
+
+bash build-dclookup.sh
+haxelib newrepo && haxelib install all --always
