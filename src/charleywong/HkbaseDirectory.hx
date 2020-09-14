f17942a41de663907fecd3098e854945240b6a63
Andy Li <andy@onthewings.net>
2020-09-14 17:32:20 +0800

    match hkbase directory with charley

--- src/charleywong/HkbaseDirectory.hx
+++ src/charleywong/HkbaseDirectory.hx
@@ -18,14 +18,11 @@ typedef HkbaseData = {
     var type:String;
     var email:String;
     var tel:String;
-    var website:String;
-    var social1:String;
-    var social2:String;
-    var social3:String;
+    var webpages:Array<String>;
     var description:String;
 }
 
-typedef HkbaseDump = DynamicAccess<HkbaseData>;
+typedef HkbaseDump = Array<HkbaseData>;
 
 class HkbaseDirectory {
     static final sheetId = "1tZ-ODQRQFI1bzvekxkR0g4eULcPk9D3BZTOYbvYWdt8";
@@ -37,8 +34,12 @@ class HkbaseDirectory {
         Json.parse(File.getContent(localCacheFile));
     } catch (e:Dynamic) {
         trace('Unable to load $localCacheFile');
-        {};
+        [];
     }
+
+    static public var entities(get, null):Array<Entity>;
+    static function get_entities()
+        return entities != null ? entities : entities = entityIndex.entities.filter(e -> e.tags.exists(t -> t.id == "hkbaseDirectory"));
 
     static function strCol(row:Dynamic, colName:String):Null<String> {
         return switch (row.field(colName)) {
@@ -63,6 +64,18 @@ class HkbaseDirectory {
             str = "http://" + str;
 
         return UrlExtractors.cleanUrl(str);
+    }
+
+    static function telCol(row:Dynamic, colName:String):Null<String> {
+        var str = strCol(row, colName);
+        if (str == null)
+            return str;
+
+        str = ~/[^0-9]/g.replace(str, "");
+        if (!~/[0-9]{8}/.match(str))
+            return null;
+
+        return str;
     }
 
     static function floatCol(row:Dynamic, colName:String):Null<Float> {
@@ -92,11 +105,13 @@ class HkbaseDirectory {
                     name_en: strCol(row, "公司名稱(英文)"),
                     type: strCol(row, "業務性質"),
                     email: strCol(row, "電郵"),
-                    tel: strCol(row, "電話"),
-                    website: urlCol(row, "網址"),
-                    social1: urlCol(row, "社交媒體(1)"),
-                    social2: urlCol(row, "社交媒體(2)"),
-                    social3: urlCol(row, "社交媒體(3)"),
+                    tel: telCol(row, "電話"),
+                    webpages: [
+                        urlCol(row, "網址"),
+                        urlCol(row, "社交媒體(1)"),
+                        urlCol(row, "社交媒體(2)"),
+                        urlCol(row, "社交媒體(3)"),
+                    ].filter(v -> v != null),
                     description: strCol(row, "HKBASE推介"),
                 }
             ])
@@ -123,7 +138,49 @@ class HkbaseDirectory {
             .then(_ -> dumpToFile());
     }
 
+    static public function test() {
+        for (e in entities) {
+            var data = getData(e);
+            if (data == null)
+                throw 'There is no HKBASE data for ' + e.id;
+        }
+
+        for (data in localCache) {
+            var e = getEntity(data);
+            if (e == null)
+                throw 'There is no entity for ' + data;
+        }
+    }
+
+    static public function match(data:HkbaseData, e:Entity):Bool {
+        if (e.webpages.exists(p -> data.webpages.has(p.url)))
+            return true;
+
+        if (e.webpages.exists(p -> p.url == 'tel:' + data.tel))
+            return true;
+
+        if (e.webpages.exists(p -> p.url == 'mailto:' + data.email))
+            return true;
+
+        return false;
+    }
+
+    static public function getEntity(data:HkbaseData):Null<Entity> {
+        return entities.find(e -> match(data, e));
+    }
+
+    static public function getData(e:Entity):Null<HkbaseData> {
+        return localCache.find(data -> match(data, e));
+    }
+
     static function main():Void {
-        dump();
+        switch (Sys.args()) {
+            case ["dump"]:
+                dump();
+            case ["test"]:
+                test();
+            case _:
+                throw "unknown args";
+        }
     }
 }
