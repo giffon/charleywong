cc3be157698c0150bf18ecbadc071aa8cc98802d
Andy Li <andy@onthewings.net>
2020-09-15 00:03:44 +0800

    add intro to hkbase directory page

--- src/charleywong/views/HkbaseDirectoryView.hx
+++ src/charleywong/views/HkbaseDirectoryView.hx
@@ -12,6 +12,10 @@ class HkbaseDirectoryView extends View {
 
     static public final path = "page/hkbase-directory";
 
+    override function bodyClasses() {
+        return super.bodyClasses().concat(["page-hkbase-directory"]);
+    }
+
     public var entities(get, never):Array<Entity>;
     function get_entities() return props.entities;
 
@@ -22,7 +26,6 @@ class HkbaseDirectoryView extends View {
     function renderEntity(e:Entity) return jsx('
         <li key=${e.id} className=${Tag.expend(e.tags).map(t -> "tag-" + t.id).join(" ")} >
             <a href=${'/${e.id}'}>${e.name.printAll()}</a>
-            <span>${e.areas().map(renderAreasLabel)}</span>
         </li>
     ');
 
@@ -41,6 +44,21 @@ class HkbaseDirectoryView extends View {
                     <header className="p-3 p-md-4 text-center">
                         <a href="/"><img className="col-4 col-md-3 col-lg-2 logo-header" src=${R("/images/charley-600-w.png")} alt="Charley Wong 和你查"/></a>
                     </header>
+                    <div className="col-12 col-md-8 mx-auto color_black_o60">
+                        <h1>《HKBASE企業名冊》名單</h1>
+                        <p>
+                            <a href="/hkbase.org">香港永續經濟聯會(HKBASE)</a> 是新時代的企業網絡，成立於2017年12月，旨在凝聚不同界別的企業人士，以推動「永續經濟 香港自強」為使命。
+                        </p>
+                        <p>
+                            2020年初起，HKBASE成立了「HKBASE商務引薦平台」，推動會員企業互相引薦供應商、客戶和伙伴，做到「香港人生意、香港人引薦」。
+                        </p>
+                        <p>
+                            HKBASE編製了這份<a href="https://www.facebook.com/hkbase.org/posts/2556063708038307" target="_blank">《HKBASE企業名冊》</a>，向公眾介紹各會員企業的產品和服務。
+                        </p>
+                        <p>
+                            以下資料由 HKBASE 提供。
+                        </p>
+                    </div>
                     <div className="col-12 col-md-8 mx-auto bg-white py-3 rounded-10 container-btm">
                         <div className="d-flex align-items-center">
                             <div className="col p-0">共有 ${entities.length} 項資料</div>
