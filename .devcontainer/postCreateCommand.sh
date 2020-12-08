9ad99323fb798b9a49c8916014bbff8b3ce78539
Andy Li <andy@onthewings.net>
2020-12-08 17:05:11 +0800

    we don't use python

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,11 +2,6 @@
 
 set -ex
 
-python3 -m virtualenv -p /usr/bin/python3.7 ~/venv
-
-echo "source ~/venv/bin/activate" >> ~/.bashrc
-bash -ic "pip3 install -r requirements.txt"
-
 sudo chmod a+rwx node_modules
 yarn
 
