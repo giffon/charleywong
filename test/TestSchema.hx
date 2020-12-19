81e289b245680b230ffc715705aff14afab1bfd2
Andy Li <andy@onthewings.net>
2020-12-19 14:36:40 +0800

    fix schema validation command

--- test/TestSchema.hx
+++ test/TestSchema.hx
@@ -6,16 +6,8 @@ using Lambda;
 using StringTools;
 
 class TestSchema extends utest.Test {
-    final schemas = "data/schema/*.json";
-
-    function testSchema() {
-        var p = new sys.io.Process("npx", ["ajv", "compile", "-s", schemas, "-r", schemas]);
-        var code = p.exitCode();
-        Assert.equals(0, code, p.stderr.readAll().toString());
-    }
-
     function testEntitiesAgainstSchema() {
-        var p = new sys.io.Process("npx", ["ajv", "-d", "data/entity/*.json", "-s", "data/schema/Entity.json", "-r", schemas]);
+        var p = new sys.io.Process("npx", ["ajv", "-d", "data/entity/*.json", "-s", "data/schema/Entity.json", "-r", "data/schema/!(Entity).json"]);
         var code = p.exitCode();
         Assert.equals(0, code, p.stderr.readAll().toString());
     }
