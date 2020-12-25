76f1097c4d2e94004fc1e6b966c11a5bd7396b4c
Andy Li <andy@onthewings.net>
2020-12-26 02:47:22 +0800

    avoid chunked transfer
    https://github.com/dougmoscrop/serverless-http/issues/142

--- src/charleywong/ReplyTools.hx
+++ src/charleywong/ReplyTools.hx
@@ -9,16 +9,10 @@ class ReplyTools {
             props = {};
 
         return try {
-            var pass = new js.node.stream.PassThrough();
-            pass.write("<!DOCTYPE html>");
             var element = React.createElement(view, props);
-            var stream = ReactDOMServer.renderToStaticNodeStream(element);
-            stream.once("error", function(e) throw e);
-            stream.once("end", function() pass.end());
-            stream.pipe(pass);
             reply
                 .header("Content-Type", 'text/html')
-                .send(pass);
+                .send("<!DOCTYPE html>" + ReactDOMServer.renderToStaticMarkup(element));
         } catch (e:Dynamic) {
             throw e;
         }
