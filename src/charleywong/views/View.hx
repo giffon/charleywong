68e54074fb15b82b8ab1ce4f011a36a276b91e17
Andy Li <andy@onthewings.net>
2021-06-09 17:06:11 +0800

    remove instant.page

--- src/charleywong/views/View.hx
+++ src/charleywong/views/View.hx
@@ -43,7 +43,6 @@ class View extends ReactComponent {
     function footJs() return jsx('
         <Fragment>
             <script src=${R("/trackExceptions.js")}></script>
-            <script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"></script>
         </Fragment>
     ');
 
