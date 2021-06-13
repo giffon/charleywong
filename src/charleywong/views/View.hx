cdcbd0cf7a8e650b2b034f70568339875615442f
Andy Li <andy@onthewings.net>
2021-06-13 23:41:47 +0800

    add a small-screen version ad

--- src/charleywong/views/View.hx
+++ src/charleywong/views/View.hx
@@ -141,7 +141,8 @@ class View extends ReactComponent {
             <div className="mt-3 md:mt-12 text-center container max-w-screen-lg mx-auto px-4 md:px-8">
                 <div className="w-full">
                     <a href="https://linktr.ee/blackwindow">
-                        <img className="max-w-full h-auto inline" src=${R("/images/blackwindow___poster_banner1_970.90.jpg")} alt="黑窗里（前蘇波榮）重開眾籌"/>
+                        <img className="max-w-full h-auto inline hidden lg:block" src=${R("/images/blackwindow___poster_banner1_970.90.jpg")} alt="黑窗里（前蘇波榮）重開眾籌"/>
+                        <img className="max-w-full h-auto inline block lg:hidden" src=${R("/images/blackwindow___poster_banner4_970.90.jpg")} alt="黑窗里（前蘇波榮）重開眾籌"/>
                     </a>
                 </div>
                 <div className="w-full mt-2 color_black_o60">
