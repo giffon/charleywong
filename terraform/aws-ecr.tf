e9aa0cd3b23b8ac59b070eb97daea4926ae3d477
Andy Li <andy@onthewings.net>
2022-01-03 02:21:45 +0800

    create aws_ecr_lifecycle_policy

--- terraform/aws-ecr.tf
+++ terraform/aws-ecr.tf
@@ -7,3 +7,45 @@ resource "aws_ecr_repository" "serverless-charleywong-production" {
   name                 = "serverless-charleywong-production"
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ecr_lifecycle_policy" "serverless-charleywong-master" {
+  repository = aws_ecr_repository.serverless-charleywong-master.name
+
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep last 5 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 5
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_ecr_lifecycle_policy" "serverless-charleywong-production" {
+  repository = aws_ecr_repository.serverless-charleywong-production.name
+
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep last 10 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+}
