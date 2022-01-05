6a78fa04627cb5c8bf047e2d1d9393f87c378f05
Andy Li <andy@onthewings.net>
2022-01-05 21:19:31 +0800

    梅貴緣 x 埗兵 埗埗糕昇大行動

--- src/charleywong/views/View.hx
+++ src/charleywong/views/View.hx
@@ -150,9 +150,8 @@ class View extends ReactComponent {
             </div>
             <div className="mt-3 md:mt-12 text-center container max-w-screen-lg mx-auto px-4 md:px-8">
                 <div className="w-full">
-                    <a href="https://ssprangers.com/">
-                        <img className="w-full hidden sm:inline" src=${R("/images/good-970-90.jpg")} alt="深水埗黃店美食 埗兵外賣俾你食"/>
-                        <img className="w-full inline sm:hidden" src=${R("/images/good-320-50.jpg")} alt="深水埗黃店美食 埗兵外賣俾你食"/>
+                    <a href="https://www.facebook.com/hkssprangers/posts/436148431522058">
+                        <img className="w-full inline" src=${R("/images/mui-cake-banner2.jpg")} alt="梅貴緣 x 埗兵 埗埗糕昇大行動"/>
                     </a>
                 </div>
                 <div className="w-full mt-2 color_black_o60">
