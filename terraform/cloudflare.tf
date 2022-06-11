8b303dcd001772aa39f7b37827fcf5f6eb0be8b8
Andy Li <andy@onthewings.net>
2022-06-11 13:07:03 +0800

    revert to using aws lambda, use node 14, which is what is supported by aws-lambda-ric

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -27,23 +27,15 @@ resource "cloudflare_record" "charleywong" {
   proxied = true
 }
 
-# resource "cloudflare_record" "charleywong-A" {
-#   zone_id = data.cloudflare_zone.giffonio.id
-#   name    = "charleywong"
-#   value   = "37.16.16.171"
-#   type    = "A"
-#   ttl     = 1
-#   proxied = true
-# }
+resource "cloudflare_record" "charleywong-master" {
+  zone_id = data.cloudflare_zone.giffonio.id
+  name    = "charleywong-master"
+  value   = "d-uifndvj038.execute-api.us-east-1.amazonaws.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
 
-# resource "cloudflare_record" "charleywong-AAAA" {
-#   zone_id = data.cloudflare_zone.giffonio.id
-#   name    = "charleywong"
-#   value   = "2a09:8280:1::44f4"
-#   type    = "AAAA"
-#   ttl     = 1
-#   proxied = true
-# }
 
 resource "cloudflare_record" "charleywong-dev-A" {
   zone_id = data.cloudflare_zone.giffonio.id
@@ -62,13 +54,3 @@ resource "cloudflare_record" "charleywong-dev-AAAA" {
   ttl     = 1
   proxied = true
 }
-
-resource "cloudflare_record" "charleywong-master" {
-  zone_id = data.cloudflare_zone.giffonio.id
-  name    = "charleywong-master"
-  value   = "d-uifndvj038.execute-api.us-east-1.amazonaws.com"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
-
